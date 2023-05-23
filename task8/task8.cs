42a936bd9eb640d7d0dc1f7c73343371bc0f2a0d
Egor <egoriq43@gmail.com>
2023-05-23 23:59:19 +0700

    task_8_done

---
+++ task8/task8.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+  int i = 1;
+bool not = true;
+
+Console.WriteLine("Enter number:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Even numbers from 1 to " + num);
+    while (i <= num)
+{
+     if (i % 2 != 1)
+       {
+        Console.Write(i + ", ");
+        not = false;
+        }
+    i++;
+}
+
+    if (not)
+{
+    Console.WriteLine("There are no even numbers in this range.");
+}
