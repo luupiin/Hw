6c4d1e93f5b94629fe69053316ac9b1063618bcb
Egor <egoriq43@gmail.com>
2023-06-01 22:04:45 +0700

    task19

---
+++ task19/task19.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Enter a number: ");
+string number = Console.ReadLine();
+int len = number.Length;
+
+if (len == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($" {number} - Palindrome");
+    }
+    else
+    {
+        Console.WriteLine($" {number} - Not a palindrome");
+    }
+}
+else
+{
+    Console.WriteLine($" ERROR: {number} - is not a five-digit number");
+}
