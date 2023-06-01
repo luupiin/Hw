8ffd59f0d9b15d19269d15e06307965b64dafad2
Egor <egoriq43@gmail.com>
2023-06-01 22:05:37 +0700

    task23

---
+++ task23/task23.cs
@@ -0,0 +1,13 @@
+﻿
+int number = ReadInt("Enter the number N: ");
+
+for (int i = 1; i <= number; i++)
+{ 
+    Console.Write($"{i*i} ");
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
