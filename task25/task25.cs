75045bc706f504bf13887a3e50e082b59b0186f8
Egor <egoriq43@gmail.com>
2023-06-05 21:10:50 +0700

    task_25_done

---
+++ task25/task25.cs
@@ -0,0 +1,16 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.WriteLine("Enter number A");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter number B");
+int b = Convert.ToInt32(Console.ReadLine());
+int step = a ;
+
+for (int i = 1; i < b; i++)
+{
+
+step = step * a ;
+
+}
+
+Console.WriteLine("A to the power of B is equal to:" + step);
