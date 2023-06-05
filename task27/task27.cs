ac5b2a414e30181245dcb9ccfd6a22041aa3f13f
Egor <egoriq43@gmail.com>
2023-06-05 21:14:36 +0700

    task_27_done

---
+++ task27/task27.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Enter a number");
+int i = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+while (i > 0)
+{
+int num = i % 10;
+i = i / 10;
+sum = sum + num;
+}
+Console.WriteLine("The sum of all digits in the number is equal to:" + sum);
