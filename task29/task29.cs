79a7ee01c69ccad6385a49a3561003214ce5a88b
Egor <egoriq43@gmail.com>
2023-06-05 21:34:55 +0700

    task_29_done

---
+++ task29/task29.cs
@@ -0,0 +1,14 @@
+﻿int [] numbers = new int[8];
+
+
+for (int i = 0; i < numbers.Length; i++)
+ {
+    numbers [i] = new Random().Next(0, 11);
+    Console.Write(" " + Method (i) + " ");
+ }
+
+int Method (int a)
+{
+    return numbers[a];
+}
+
