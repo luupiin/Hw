6aab5b19d531229af99d887cdd4b8c1f2b617153
Egor <egoriq43@gmail.com>
2023-06-13 21:33:54 +0700

    task_43_done

---
+++ task43/task43.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("enter a value b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter a number k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter a value b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter a number k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
+
+Console.WriteLine($"Two straight lines will intersect at a point with X coordinates: {x}, Y: {y}");
